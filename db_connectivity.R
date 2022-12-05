b8265f06ca50c3af73a01b713eeff1d4e17154dd
MegaBytten <omegabytten@gmail.com>
2022-12-05 11:04:06 +0000

    05/12/22 - MySQL Attempt  - Chatted to Katie and now understand that they don’t pull data out of MySQL into a df like I do  - Found RMySQL package that establishes database connections    > Can pull and push data to local Slade MySQL whilst running on Slade    > Created a test .R document ‘dv_connectivity.R’ that attempts to push 1 trial table to the MySQL DB so I know it works    > Fixed Security issue, must run script in interactive session to allow user input for MySQL: DB, username and password      ~ Prevents uploading credentials to Github      ~ Can securely still work with MySQL DB

---
+++ db_connectivity.R
@@ -0,0 +1,22 @@
+library(devtools) # Essential for Aurum integration
+library(tidyverse)  # streamlining R coding since 2016
+library(r2r) # Used in Hashmaps later
+library(RMySQL)
+
+db.username = readline(prompt="Enter MySQL Username: ")
+db.password = readline(prompt="Enter MySQL Password: ")
+db.database = readline(prompt="Enter Target Database: ")
+
+# Connect to a MySQL database running locally
+con = dbConnect(RMySQL::MySQL(), dbname = db.database, user = db.username, password = db.password) # host = db.host
+
+#Lists
+Keys = c('This', 'is', 'a', 'test', 'dataframe')
+Values = c(1:5)
+
+df = data.frame(Keys, Values)
+View(df)
+
+dbWriteTable(conn = con, name = 'ethan_test_dbconnection', value = df)
+# Always cleanup by disconnecting the database
+dbDisconnect(con)
